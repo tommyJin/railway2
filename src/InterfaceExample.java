b623764a1c3c4ad6ff94022b92ffd9f843f08b78
tommyJin <tommyjing0620@gmail.com>
2016-03-04 17:42:13 +0000

    generate a journey passby automatically

--- src/InterfaceExample.java
+++ src/InterfaceExample.java
@@ -1,9 +1,6 @@
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by tommy on 03/03/2016 20:39
@@ -42,15 +39,38 @@ public class InterfaceExample {
         journeys.add(interlock.addJourney(railway,"j3", "s8", "s2", "s8;s3;s2"));
         journeys.add(interlock.addJourney(railway,"j4", "s8", "s2", "s8;s5;s2"));*/
 
-/*        //map2
+        //map2
         journeys.add(interlock.addJourney(railway,"j1",  "s1", "s11", "s1;s4;s8;s11"));
         journeys.add(interlock.addJourney(railway,"j2",  "s1", "s11", "s1;s6;s10;s11"));
         journeys.add(interlock.addJourney(railway,"j3",  "s12", "s2", "s12;s9;s3;s2"));
-        journeys.add(interlock.addJourney(railway,"j4",  "s12", "s2", "s12;s9;s5;s2"));*/
+        journeys.add(interlock.addJourney(railway,"j4",  "s12", "s2", "s12;s9;s5;s2"));
+        journeys.add(interlock.addJourney(railway,"j3",  "s12", "s2", "s12;s7;s3;s2"));
+        journeys.add(interlock.addJourney(railway,"j4",  "s12", "s2", "s12;s7;s5;s2"));
 
         //pass a journey list to the program to let it run
-//        railway.setJourneys(journeys);
+        railway.setJourneys(journeys);
+
+        String passby = generateJourney(railway,passbys);
+        System.out.println("Chosen by me : "+passby);
+
 //        List<Railway> railways = interlock.running(railway);
     }
 
+    public static String generateJourney(Railway railway,List<String> passbys){
+        List<Journey> journeys = railway.getJourneys();
+        List<String> exits = new ArrayList<>();
+        for (int i = 0; i < journeys.size(); i++) {
+            List<Route> routes = journeys.get(i).getRoutes();
+            String passby = journeys.get(i).getSource();
+            for (int j = 0; j < routes.size(); j++) {
+                passby += ";"+routes.get(j).getDest();
+            }
+            exits.add(passby);
+        }
+
+        passbys.removeAll(exits);
+
+        Random random = new Random();
+        return passbys.size()==0?exits.get(random.nextInt(exits.size())):passbys.get(random.nextInt(passbys.size()));
+    }
 }
